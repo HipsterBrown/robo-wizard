a636aeab280a991652a20da47c00a4d755ee8f98
Nick Hehr <nicholas.hehr@betterment.com>
2020-12-29 14:12:54 -0500

    refactor: replace robot3 with @xstate/fsm

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-import {
-  createMachine,
-  transition,
-  guard,
-  reduce,
-  interpret,
-  state,
-  Transition,
-  Guard,
-  Action,
-  Reducer,
-  Machine,
-  MachineState,
-  Service,
-} from 'robot3';
+import { createMachine, interpret, StateMachine, assign } from '@xstate/fsm';
 
 /** Base type describing an object of string keys and `any` value */
-export type BaseValues = {
-  [key: string]: any;
-};
+export type BaseValues = object;
 
 /**
  * @typeParam Values Generic type for object of values being gathered through the wizard steps
@@ -32,14 +16,14 @@ export type BaseValues = {
  * ] // StepTransition[]
  * ```
  */
-export type StepTransition<Values = BaseValues> =
+export type StepTransition<Values extends object = BaseValues> =
   | string
-  | Array<string | Guard<Values> | Reducer<Values> | Action<Values>>;
+  | Array<string | { cond: WhenFunction<Values> }>;
 
 /**
  * @typeParam Values Generic type for object of values being gathered through the wizard steps
  */
-export type StepConfig<Values = BaseValues> = {
+export type StepConfig<Values extends object = BaseValues> = {
   /** Name of the step */
   name: string;
   /**
@@ -61,7 +45,18 @@ export type StepConfig<Values = BaseValues> = {
  *
  * A step in the wizard can be described as a string for default progression, or an object for custom progression, see [[StepConfig]] for more details
  */
-export type FlowStep<Values = BaseValues> = string | StepConfig<Values>;
+export type FlowStep<Values extends object = BaseValues> =
+  | string
+  | StepConfig<Values>;
+
+type WizardEvent<Values extends object> =
+  | {
+      type: 'next';
+      values?: Partial<Values>;
+    }
+  | {
+      type: 'previous';
+    };
 
 /**
  * @typeParam StepMachine Generic type for the configured state machine, based on [[Machine]] from [robot](https://thisrobot.life)
@@ -70,9 +65,10 @@ export type FlowStep<Values = BaseValues> = string | StepConfig<Values>;
  *
  * An event handler for reacting when the wizard updates, i.e. after step progression or values have been updated
  */
-type ChangeHandler<StepMachine extends Machine, Values = BaseValues> = (
-  wizard: RoboWizard<StepMachine, Values>
-) => void;
+type ChangeHandler<
+  StepMachine extends StateMachine.Machine<Values, WizardEvent<Values>, any>,
+  Values extends object = BaseValues
+> = (wizard: RoboWizard<StepMachine, Values>) => void;
 
 /**
  * @typeParam StepMachine Generic type for the configured state machine, based on the `Machine` type from [robot](https://thisrobot.life)
@@ -80,9 +76,12 @@ type ChangeHandler<StepMachine extends Machine, Values = BaseValues> = (
  *
  * This class is the return value from [[createWizard]] and the only way to be instantiated.
  */
-class RoboWizard<StepMachine extends Machine, Values = BaseValues> {
+class RoboWizard<
+  StepMachine extends StateMachine.Machine<Values, WizardEvent<Values>, any>,
+  Values extends object = BaseValues
+> {
   /** @ignore */
-  private _service?: Service<StepMachine>;
+  private _service?: StateMachine.Service<Values, WizardEvent<Values>, any>;
 
   /** @ignore */
   constructor(private machine: StepMachine) {}
@@ -95,28 +94,26 @@ class RoboWizard<StepMachine extends Machine, Values = BaseValues> {
    */
   public start(onChange: ChangeHandler<StepMachine, Values>, values?: Values) {
     if (this._service) return;
-    this._service = interpret<StepMachine, 'next' | 'previous'>(
-      this.machine,
-      () => {
-        onChange(this);
-      },
-      values || this.machine.context
-    );
-    onChange(this);
+    this._service = interpret<Values, WizardEvent<Values>>(this.machine);
+    this._service.subscribe(() => onChange(this));
+    this._service.start({
+      context: values || this.machine.config.context,
+      value: this.machine.initialState.value,
+    });
   }
 
   /**
    * Returns the current step of the wizard
    */
   get currentStep() {
-    return this.service.machine.current;
+    return this.service.state.value;
   }
 
   /**
    * Returns the current state of values been gathered through the wizard
    */
   get currentValues() {
-    return this.service.context;
+    return this.service.state.context;
   }
 
   /**
@@ -245,104 +242,79 @@ class RoboWizard<StepMachine extends Machine, Values = BaseValues> {
  * console.log(wizard.currentStep); // third
  * ```
  */
-export function createWizard<Values = BaseValues>(
+export function createWizard<Values extends object = BaseValues>(
   steps: FlowStep<Values>[],
   initialValues: Values = {} as Values
 ) {
   const normalizedSteps: StepConfig<Values>[] = steps.map(step =>
     typeof step === 'string' ? { name: step } : step
   );
-
-  const stepConfig = Object.freeze(
-    normalizedSteps.reduce<{
-      [step: string]: MachineState;
-    }>((config, step, index) => {
-      const transitions: Transition[] = [];
-
-      if (step.previous && typeof step.previous === 'string') {
-        transitions.push(transition('previous', step.previous));
-      } else if (
-        step.previous !== false &&
-        step.name !== normalizedSteps[0].name
-      ) {
-        transitions.push(
-          transition('previous', normalizedSteps[index - 1].name)
-        );
-      }
-
-      if (step.next) {
-        if (typeof step.next === 'string') {
-          transitions.push(
-            transition('next', step.next, updateValues<Values>())
-          );
-        } else if (Array.isArray(step.next)) {
-          transitions.push(
-            ...step.next.map(nextStep => {
-              const [stepName, ...guardsOrReducers] =
-                typeof nextStep === 'string' ? [nextStep] : nextStep;
-              return transition(
-                'next',
-                stepName as string,
-                ...(guardsOrReducers as Array<
-                  Guard<Values> | Reducer<Values> | Action<Values>
-                >),
-                updateValues<Values>()
-              );
-            })
-          );
-        }
-      } else if (
-        step.next !== false &&
-        step.name !== normalizedSteps[steps.length - 1].name
-      ) {
-        transitions.push(
-          transition(
-            'next',
-            normalizedSteps[index + 1].name,
-            updateValues<Values>()
-          )
-        );
-      }
-
-      return {
-        ...config,
-        [step.name]: state(...transitions),
+  const config: StateMachine.Config<Values, WizardEvent<Values>> = {
+    id: 'robo-wizard',
+    initial: normalizedSteps[0].name,
+    context: initialValues,
+    states: normalizedSteps.reduce<
+      StateMachine.Config<Values, WizardEvent<Values>>['states']
+    >((result, step, index) => {
+      result[step.name] = {
+        on: {
+          previous: {
+            target:
+              typeof step.previous === 'string'
+                ? step.previous
+                : step.previous !== false &&
+                  step.name !== normalizedSteps[0].name
+                ? normalizedSteps[index - 1].name
+                : undefined,
+          },
+          next: Array.isArray(step.next)
+            ? step.next.map(nextStep => {
+                const [stepName, guard] =
+                  typeof nextStep === 'string' ? [nextStep] : nextStep;
+                const { cond } =
+                  typeof guard === 'object' ? guard : { cond: () => true };
+                return {
+                  target: typeof stepName === 'string' ? stepName : undefined,
+                  cond,
+                  actions: assign((context, event) => ({
+                    ...context,
+                    ...event.values,
+                  })),
+                };
+              })
+            : {
+                target:
+                  typeof step.next === 'string'
+                    ? step.next
+                    : step.next !== false &&
+                      step.name !== normalizedSteps[steps.length - 1].name
+                    ? normalizedSteps[index + 1].name
+                    : undefined,
+                actions: assign((context, event) => ({
+                  ...context,
+                  ...event.values,
+                })),
+              },
+        },
       };
-    }, {})
-  );
-  const machine = createMachine(stepConfig, () => initialValues) as Machine<
-    typeof stepConfig,
-    Values,
-    string
-  >;
-
+      return result;
+    }, {}),
+  };
+  const machine = createMachine(config);
   return new RoboWizard<typeof machine, Values>(machine);
 }
 
 /**
  * Event object containing any new values to be updated
  */
-type UpdateEvent<Values> = { values: Values };
-
-/**
- * @internal
- */
-function updateValues<Values = BaseValues>() {
-  return reduce<Values>((currentValues, event) => {
-    const newValues = (event as UpdateEvent<Values>).values || {};
-    return {
-      ...currentValues,
-      ...newValues,
-    };
-  });
-}
+type UpdateEvent<Values> = { values?: Partial<Values> };
 
 /**
  * @typeParam Values Generic type for object of values being gathered through the wizard steps
  * @param values Current state of values gathered through the wizard
  * @param event Event object containing any new values to be updated, see [[UpdateEvent]]
  */
-export type WhenFunction<Values = BaseValues> = (
+export type WhenFunction<Values extends object = BaseValues> = (
   values: Values,
   event: UpdateEvent<Values>
 ) => boolean;
@@ -352,8 +324,10 @@ export type WhenFunction<Values = BaseValues> = (
  * @param fn Guard function to be called to test if the step should transition, see [[WhenFunction]]
  * See [[createWizard]] for example usage
  */
-export function when<Values = BaseValues>(fn: WhenFunction<Values>) {
-  return guard<Values>((context, event) =>
-    fn(context, event as UpdateEvent<Values>)
-  );
+export function when<Values extends object = BaseValues>(
+  cond: WhenFunction<Values>
+) {
+  return {
+    cond,
+  };
 }

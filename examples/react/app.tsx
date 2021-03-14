51c05459412115228bc925342b1671ac5189a64e
HipsterBrown <headhipster@hipsterbrown.com>
2021-03-14 13:00:00 -0400

    chore: pin all parcel versions

--- examples/react/app.tsx
+++ examples/react/app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { createWizard, FlowStep, BaseValues } from '../../src/index';
 
-function useWizard<Values = BaseValues>(
+function useWizard<Values extends object = BaseValues>(
   steps: FlowStep<Values>[],
   initialValues: Values = {} as Values
 ) {
